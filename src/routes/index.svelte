fe640ad42d8a3ec6d5a211bb29656ba5f2a2e020
Ryu-134 <caseydane134@gmail.com>
2024-11-21 23:00:27 -0800

    feat(src/routes/index.svelte): Create main index.svelte file

---
+++ src/routes/index.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { goto } from '$app/navigation'; 
+</script>
+
+<h1>Data Structures & Algorithms Visualizer</h1>
+<p>Select a DSA to visualize:</p>
+
+<ul>        // Add options for different DSA
+    <li><button on:click={() => goto('/vector')}>Vector</button></li>   //when function runs '()' call goto function passing parameter /vector
+</ul>
