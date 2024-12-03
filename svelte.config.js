0933f7eaa9fe527df0c6073b165a087a8c085843
Ryu-134 <caseydane134@gmail.com>
2024-12-03 00:19:01 -0800

    Create new branch for static build

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,7 +11,15 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+            // Options for adapter-static
+            pages: 'build',  // Output directory for GitHub Pages
+            assets: 'build', // Directory for assets
+            fallback: 'index.html'   // 'index.html' if you need single-page app behavior
+		}),
+		paths: {
+			base: '',	// set this if deploying to subdirectory in the future
+		}
 	}
 };
 
